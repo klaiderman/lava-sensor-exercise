1adb7e6b20afd2ec0e67bdec0b92098c870cb013
klaiderman <63550727+klaiderman@users.noreply.github.com>
2026-09-09 05:15:54 +0300

    chore: drop reviewer scratch copy of the module from history; ignore reports/review scratch

--- reports/review/sensorcopy/internal/probe/fifo_other_test.go
+++
@@ -1,7 +0,0 @@
-//go:build !unix
-
-package probe
-
-import "errors"
-
-func mkfifo(string) error { return errors.New("mkfifo is not available on this platform") }
--- reports/review/sensorcopy/internal/probe/fifo_unix_test.go
+++
@@ -1,7 +0,0 @@
-//go:build unix
-
-package probe
-
-import "syscall"
-
-func mkfifo(p string) error { return syscall.Mkfifo(p, 0o600) }
